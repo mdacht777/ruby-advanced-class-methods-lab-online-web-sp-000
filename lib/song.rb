2ad1b5abaa97acce14a75c93bef7d276b63cdd1f
Martin Achtermann <mdacht777@gmail.com>
2020-06-21 15:08:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,4 +35,10 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    if !self.all.find{|person| person.name == name}
+      self.create_by_name(name)
+    end
+  end
+
 end
